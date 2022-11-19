426749fc5fe60d0f5a992cd431d9680de5434cca
xiaoxuin <118498435+xiaoxuin@users.noreply.github.com>
2022-11-19 08:49:04 +0800

    pull-commit

--- src/main/java/com/pzx/git/GitTest.java
+++ src/main/java/com/pzx/git/GitTest.java
@@ -8,7 +8,7 @@ public class GitTest {
         System.out.println("hello.git4");
         System.out.println("master");
         System.out.println("hot-fix-test");
-        System.out.println("push-test");
-
+        System.out.println("push-test");    
+        System.out.println("pull-test");  
     }
 }
